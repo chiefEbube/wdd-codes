8321451d83576d136e816f94da378c83cb42db61
chiefebube <ebubechi.anyanwu@yahoo.com>
2024-02-08 10:21:58 +0100

    Class six template

---
+++ class-five/script.js
@@ -0,0 +1,15 @@
+let davidAge = 5; // Number
+
+let davidLastName = 'Belema'; // String
+
+let isDavidMarried = true; // Boolean
+
+let felicityHeight = 6;
+
+// if (isDavidMarried === true){
+//     davidLastName = 'Israel'
+// }
+
+const add = davidAge + felicityHeight;
+
+console.log(add)
